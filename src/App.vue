eb65ebe90cead73367c226f471913b5d7806118c
Vahidone <v.ban.roma@gmail.com>
2023-10-16 18:03:19 +0200

    feat: add call emit in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,63 @@
-
 <script>
-import axios from 'axios';
-import { store } from "./data/store";
-import Header from './components/Header.vue';
-import Main from './components/Main.vue';
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Main,
+import { store } from './data/store';
+import axios from "../node_modules/axios";
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
 
+
+export default {
+  name: "App",
+  components:{
+    Header,
+    Main
   },
-  data () {
+  data() {
     return {
       store
     }
   },
 
-  methods: {
-    getApi() {
-      axios.get(store.apiUrlBase)
-      .then((res) => {
 
+
+  methods: {
+    getApi(apiUrl){
+
+      axios.get(apiUrl,{
+        params:{
+          query: store.query,
+          api_key: store.apiKey,
+          language: store.language,
+          
+        }
       })
-      .catch((err) => {
-        console.log(err);
+      .then((res) => {
+        
+        if(apiUrl.includes("movie")){
+          store.apiResFilm = res.data;
+        }
+        if(apiUrl.includes("tv")){
+          store.apiResSerie = res.data;
+        }
+    
+      })
+
+      .catch((error) => {
+        console.log(error);
       })
     }
-    
-  }
+  },
+  mounted() {
+    this.getApi(store.apiUrlFilm);
+    this.getApi(store.apiUrlSerie);
+  },
 }
 </script>
 
 <template>
-  <Header />
+  <Header @startSearch="getApi(store.apiUrlFilm)" />
   <Main />
 </template>
 
-
-<style lang="scss"> 
-@use "../scss/main.scss" as *;
-
+<style lang="scss">
+@use "./scss/main.scss"
 </style>
