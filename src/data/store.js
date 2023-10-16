57b1784201a4d05391fc82aa4d0af8117945054e
Vahidone <v.ban.roma@gmail.com>
2023-10-16 20:34:42 +0200

    refactor: refact code in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  apiKey: "8c3144caf08ff7ccb880d95e80b23236",
+  apiKey: "9d2fe7e840b69f999f50c8cb4c989a01",
   
   apiUrlSerie: "https://api.themoviedb.org/3/search/tv",
   apiResSerie: {},
@@ -10,7 +10,7 @@ export const store = reactive({
   
   language: "it_IT",
 
-  query:"Red",
+  query:"Blue",
  
   
 })
