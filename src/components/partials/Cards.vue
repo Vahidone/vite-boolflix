bc0d0e8773422140fc92796596b172d487389339
Vahidone <v.ban.roma@gmail.com>
2023-10-16 18:51:42 +0200

    style: change size of hover

--- src/components/partials/Cards.vue
+++ src/components/partials/Cards.vue
@@ -1,16 +1,42 @@
-
 <script>
 export default {
-  name: 'Cards'
-
+  name: "Card",
+  props:{
+    card: Object,
+  }
 }
 </script>
 
 <template>
-  <h2>Cards</h2>
+  <div class="my-card">
+    <img :src="`https://image.tmdb.org/t/p/w342${card.poster_path}`" alt="">
+    <div class="my-hover">
+      <p class="mt-5">Nome	:	{{ card.title }}</p>
+      <p>Titolo originale	:	{{ card.original_title }}</p>
+      <p>lingua	:	{{ card.original_language }}</p>
+      <p>Voto	:	{{ card.vote_average }}</p>
+    </div>
+  </div>
 </template>
 
-
-<style lang="scss"> 
-
+<style lang="scss" scoped>
+.my-card{
+  position: relative;
+  .my-hover{
+    position: absolute;
+    font-size: 24px;
+    text-align: center;
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, .6);
+    cursor: pointer;
+    padding: 16px;
+    &:hover{
+      opacity: 1;
+    }
+  }
+}
 </style>
