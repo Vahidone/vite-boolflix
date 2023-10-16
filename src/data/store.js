bc322c5c0755df63844d5e08bb690ed5c3daff08
Vahidone <v.ban.roma@gmail.com>
2023-10-16 17:59:57 +0200

    feat: add api film and series

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,16 @@
-import { reactive } from 'vue';
+import { reactive } from "vue";
 
-export const store = reactive ({
+export const store = reactive({
+  apiKey: "8c3144caf08ff7ccb880d95e80b23236",
+  
+  apiUrlSerie: "https://api.themoviedb.org/3/search/tv",
+  apiResSerie: {},
+  apiUrlFilm: "https://api.themoviedb.org/3/search/movie",
+  apiResFilm: {},
+  
+  language: "it_IT",
 
+  query:"one piece",
+ 
   
 })
